8a3c1ff01841201089de680980000a06d879477a
Daniel <danxemail@gmail.com>
2024-01-20 22:44:48 -0500

    Disabled indentline, and clean up various config files

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,13 +1,13 @@
 return {
-  --- Alpha
-  {
-    "goolord/alpha-nvim",
-    event = "VimEnter",
-    enabled = true,
-    init = false,
-    opts = function()
-      local dashboard = require("alpha.themes.dashboard")
-      local logo = [[
+    --- Alpha
+    {
+        "goolord/alpha-nvim",
+        event = "VimEnter",
+        enabled = true,
+        init = false,
+        opts = function()
+            local dashboard = require("alpha.themes.dashboard")
+            local logo = [[
 ██╗   ██╗██╗███╗   ███╗
 ██║   ██║██║████╗ ████║
 ██║   ██║██║██╔████╔██║
@@ -17,7 +17,7 @@ return {
 
     ]]
 
-      dashboard.section.header.val = vim.split(logo, "\n")
+            dashboard.section.header.val = vim.split(logo, "\n")
       -- stylua: ignore
       dashboard.section.buttons.val = {
         dashboard.button("f", " " .. " Find file",       "<cmd> Telescope find_files <cr>"),
@@ -30,108 +30,207 @@ return {
         dashboard.button("l", "󰒲 " .. " Lazy",            "<cmd> Lazy <cr>"),
         dashboard.button("q", " " .. " Quit",            "<cmd> qa <cr>"),
       }
-      for _, button in ipairs(dashboard.section.buttons.val) do
-        button.opts.hl = "AlphaButtons"
-        button.opts.hl_shortcut = "AlphaShortcut"
-      end
-      dashboard.section.header.opts.hl = "AlphaHeader"
-      dashboard.section.buttons.opts.hl = "AlphaButtons"
-      dashboard.section.footer.opts.hl = "AlphaFooter"
-      dashboard.opts.layout[1].val = 8
-      return dashboard
-    end,
-    config = function(_, dashboard)
-      -- close Lazy and re-open when the dashboard is ready
-      if vim.o.filetype == "lazy" then
-        vim.cmd.close()
-        vim.api.nvim_create_autocmd("User", {
-          once = true,
-          pattern = "AlphaReady",
-          callback = function()
-            require("lazy").show()
-          end,
-        })
-      end
-
-      require("alpha").setup(dashboard.opts)
-
-      vim.api.nvim_create_autocmd("User", {
-        once = true,
-        pattern = "LazyVimStarted",
-        callback = function()
-          local stats = require("lazy").stats()
-          local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
-          dashboard.section.footer.val = "⚡ Neovim loaded "
-            .. stats.loaded
-            .. "/"
-            .. stats.count
-            .. " plugins in "
-            .. ms
-            .. "ms"
-          pcall(vim.cmd.AlphaRedraw)
+            for _, button in ipairs(dashboard.section.buttons.val) do
+                button.opts.hl = "AlphaButtons"
+                button.opts.hl_shortcut = "AlphaShortcut"
+            end
+            dashboard.section.header.opts.hl = "AlphaHeader"
+            dashboard.section.buttons.opts.hl = "AlphaButtons"
+            dashboard.section.footer.opts.hl = "AlphaFooter"
+            dashboard.opts.layout[1].val = 8
+            return dashboard
         end,
-      })
-    end,
-  },
+        config = function(_, dashboard)
+            -- close Lazy and re-open when the dashboard is ready
+            if vim.o.filetype == "lazy" then
+                vim.cmd.close()
+                vim.api.nvim_create_autocmd("User", {
+                    once = true,
+                    pattern = "AlphaReady",
+                    callback = function()
+                        require("lazy").show()
+                    end,
+                })
+            end
 
-  -- Illuminate
-  {
-    "RRethy/vim-illuminate",
-    event = "LazyFile",
-    opts = {
-      delay = 200,
-      large_file_cutoff = 2000,
-      large_file_overrides = {
-        providers = { "lsp" },
-      },
-    },
-    config = function(_, opts)
-      require("illuminate").configure(opts)
+            require("alpha").setup(dashboard.opts)
 
-      local function map(key, dir, buffer)
-        vim.keymap.set("n", key, function()
-          require("illuminate")["goto_" .. dir .. "_reference"](false)
-        end, { desc = dir:sub(1, 1):upper() .. dir:sub(2) .. " Reference", buffer = buffer })
-      end
-
-      map("]]", "next")
-      map("[[", "prev")
-
-      -- also set it after loading ftplugins, since a lot overwrite [[ and ]]
-      vim.api.nvim_create_autocmd("FileType", {
-        callback = function()
-          local buffer = vim.api.nvim_get_current_buf()
-          map("]]", "next", buffer)
-          map("[[", "prev", buffer)
+            vim.api.nvim_create_autocmd("User", {
+                once = true,
+                pattern = "LazyVimStarted",
+                callback = function()
+                    local stats = require("lazy").stats()
+                    local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+                    dashboard.section.footer.val = "⚡ Neovim loaded "
+                        .. stats.loaded
+                        .. "/"
+                        .. stats.count
+                        .. " plugins in "
+                        .. ms
+                        .. "ms"
+                    pcall(vim.cmd.AlphaRedraw)
+                end,
+            })
         end,
-      })
-    end,
-    keys = {
-      { "]]", desc = "Next Reference" },
-      { "[[", desc = "Prev Reference" },
     },
-  },
 
-  --- Toggle Term
-  {
-    "akinsho/toggleterm.nvim",
-    config = true,
-    cmd = "ToggleTerm",
-    keys = { { "<F4>", "<cmd>ToggleTerm<cr>", desc = "Toggle floating terminal" } },
-    opts = {
-      open_mapping = [[<F4>]],
-      direction = "float",
-      shade_filetypes = {},
-      hide_numbers = true,
-      insert_mappings = true,
-      terminal_mappings = true,
-      start_in_insert = true,
-      close_on_exit = true,
+    -- Illuminate
+    {
+        "RRethy/vim-illuminate",
+        event = "LazyFile",
+        opts = {
+            delay = 200,
+            large_file_cutoff = 2000,
+            large_file_overrides = {
+                providers = { "lsp" },
+            },
+        },
+        config = function(_, opts)
+            require("illuminate").configure(opts)
+
+            local function map(key, dir, buffer)
+                vim.keymap.set("n", key, function()
+                    require("illuminate")["goto_" .. dir .. "_reference"](false)
+                end, { desc = dir:sub(1, 1):upper() .. dir:sub(2) .. " Reference", buffer = buffer })
+            end
+
+            map("]]", "next")
+            map("[[", "prev")
+
+            -- also set it after loading ftplugins, since a lot overwrite [[ and ]]
+            vim.api.nvim_create_autocmd("FileType", {
+                callback = function()
+                    local buffer = vim.api.nvim_get_current_buf()
+                    map("]]", "next", buffer)
+                    map("[[", "prev", buffer)
+                end,
+            })
+        end,
+        keys = {
+            { "]]", desc = "Next Reference" },
+            { "[[", desc = "Prev Reference" },
+        },
     },
-  },
 
-  --- Vista
-  {
-    "liuchengxu/vista.vim",
-  },
+    --- Toggle Term
+    {
+        "akinsho/toggleterm.nvim",
+        config = true,
+        cmd = "ToggleTerm",
+        keys = { { "<F4>", "<cmd>ToggleTerm<cr>", desc = "Toggle floating terminal" } },
+        opts = {
+            open_mapping = [[<F4>]],
+            direction = "float",
+            shade_filetypes = {},
+            hide_numbers = true,
+            insert_mappings = true,
+            terminal_mappings = true,
+            start_in_insert = true,
+            close_on_exit = true,
+        },
+    },
+
+    --- Vista
+    {
+        "liuchengxu/vista.vim",
+    },
+
+    --- Indent blankline
+    {
+        "lukas-reineke/indent-blankline.nvim",
+        event = "LazyFile",
+        opts = {
+            indent = {
+                char = "│",
+                tab_char = "│",
+            },
+            scope = { enabled = false },
+            exclude = {
+                filetypes = {
+                    "help",
+                    "alpha",
+                    "dashboard",
+                    "neo-tree",
+                    "Trouble",
+                    "trouble",
+                    "lazy",
+                    "mason",
+                    "notify",
+                    "toggleterm",
+                    "lazyterm",
+                    ".txt",
+                },
+            },
+        },
+        main = "ibl",
+        enabled = false,
+    },
+
+    --- Zen mode
+    {
+        "folke/zen-mode.nvim",
+        opts = {
+            window = {
+                backdrop = 0.95, -- shade the backdrop of the Zen window. Set to 1 to keep the same as Normal
+                -- height and width can be:
+                -- * an absolute number of cells when > 1
+                -- * a percentage of the width / height of the editor when <= 1
+                -- * a function that returns the width or the height
+                width = 130, -- width of the Zen window
+                height = 1, -- height of the Zen window
+                -- by default, no options are changed for the Zen window
+                -- uncomment any of the options below, or add other vim.wo options you want to apply
+                options = {
+                    -- signcolumn = "no", -- disable signcolumn
+                    -- number = false, -- disable number column
+                    -- relativenumber = false, -- disable relative numbers
+                    -- cursorline = false, -- disable cursorline
+                    -- cursorcolumn = false, -- disable cursor column
+                    -- foldcolumn = "0", -- disable fold column
+                    -- list = false, -- disable whitespace characters
+                },
+            },
+            plugins = {
+                -- disable some global vim options (vim.o...)
+                -- comment the lines to not apply the options
+                options = {
+                    enabled = true,
+                    ruler = false, -- disables the ruler text in the cmd line area
+                    showcmd = false, -- disables the command in the last line of the screen
+                    -- you may turn on/off statusline in zen mode by setting 'laststatus'
+                    -- statusline will be shown only if 'laststatus' == 3
+                    laststatus = 0, -- turn off the statusline in zen mode
+                },
+                twilight = { enabled = true }, -- enable to start Twilight when zen mode opens
+                gitsigns = { enabled = false }, -- disables git signs
+                tmux = { enabled = false }, -- disables the tmux statusline
+                -- this will change the font size on kitty when in zen mode
+                -- to make this work, you need to set the following kitty options:
+                -- - allow_remote_control socket-only
+                -- - listen_on unix:/tmp/kitty
+                kitty = {
+                    enabled = false,
+                    font = "+4", -- font size increment
+                },
+                -- this will change the font size on alacritty when in zen mode
+                -- requires  Alacritty Version 0.10.0 or higher
+                -- uses `alacritty msg` subcommand to change font size
+                alacritty = {
+                    enabled = false,
+                    font = "14", -- font size
+                },
+                -- this will change the font size on wezterm when in zen mode
+                -- See alse also the Plugins/Wezterm section in this projects README
+                wezterm = {
+                    enabled = false,
+                    -- can be either an absolute font size or the number of incremental steps
+                    font = "+4", -- (10% increase per step)
+                },
+            },
+            -- callback where you can add custom code when the Zen window opens
+            on_open = function(win) end,
+            -- callback where you can add custom code when the Zen window closes
+            on_close = function() end,
+        },
+    },
 }
