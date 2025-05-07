e38a88101066db56a002279ceb5d0089c24f62e5
Daniel <daniel.reyes@exodusintel.com>
2025-05-07 13:41:11 -0400

    Changed color scheme

--- lua/plugins/extras.lua
+++ lua/plugins/extras.lua
@@ -79,4 +79,12 @@ return {
             },
         },
     },
+    {
+        "xero/miasma.nvim",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            vim.cmd("colorscheme miasma")
+        end,
+    },
 }
